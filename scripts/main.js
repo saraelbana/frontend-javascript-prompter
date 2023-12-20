79ed86e842685e35c11438302ec56c4b4112e8e7
saraelbana <matika20@gmail.com>
2023-12-20 23:14:35 +0200

    finished opdracht 3

--- scripts/main.js
+++ scripts/main.js
@@ -93,3 +93,29 @@ if(departments.hasOwnProperty(departmentInput)){
 else{
     console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen");
 }
+
+
+/*
+//Opdracht 3a
+ userInput = prompt("Je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in. " +
+     '\n0:' + departments["marketing"].jobs[0].title +
+     '\n1:' + departments["marketing"].jobs[1].title +
+     '\n2:' + departments["marketing"].jobs[2].title);
+userInput = userInput.toLowerCase();
+//Opdracht 3b
+//Opdracht 3c
+switch (userInput){
+    case '0':
+        console.log("Je koos "+ departments["marketing"].jobs[0].title + " Een uitdagende rol! " +departments["marketing"].jobs[0].description);
+        ;
+        break;
+    case'1':
+        console.log("Je koos "+ departments["marketing"].jobs[1].title + " Een uitdagende rol! " +departments["marketing"].jobs[1].description);
+        break;
+    case '2':
+        console.log("Je koos "+ departments["marketing"].jobs[2].title + " Een uitdagende rol! " +departments["marketing"].jobs[2].description);
+        break;
+    default:
+        console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen");
+}
+*/
